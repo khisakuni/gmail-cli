bf520f32b6d3a70fd6e700f73f4235c1b71ebfea
Kohei Hisakuni <kohei@philosophie.is>
2017-12-03 21:37:10 -0800

    Get next batch of emails

--- gmail/table.go
+++ gmail/table.go
@@ -35,6 +35,28 @@ func cursorUp(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 
+func onClick(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		table, _ := g.SetCurrentView("table")
+		table.Clear()
+		ids, _ := getNext()
+		subjectsList = make([]string, 0)
+		if len(subjectsList) < len(ids) {
+			ch := make(chan string)
+			getSubjects(ids, ch)
+			for subject := range ch {
+				fmt.Fprintf(table, "> next: %v\n", subject)
+				subjectsList = append(subjectsList, subject)
+
+				if len(ids) == len(subjectsList) {
+					close(ch)
+				}
+			}
+		}
+	}
+	return nil
+}
+
 var subjectsList []string = make([]string, 0)
 var ids []string = make([]string, 0)
 
@@ -54,10 +76,13 @@ func main() {
 
 	g.SetManagerFunc(layout)
 
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := g.SetKeybinding("nextBtn", gocui.MouseLeft, gocui.ModNone, onClick); err != nil {
 		log.Panicln(err)
 	}
 
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
 	if err := g.SetKeybinding("table", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
 		log.Panicln(err)
 	}
@@ -66,6 +91,7 @@ func main() {
 	}
 
 	g.Cursor = true
+	g.Mouse = true
 
 	if err != nil {
 		log.Panicln(err)
@@ -81,13 +107,22 @@ func main() {
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("table", 0, 0, maxX, maxY); err != nil {
+	if v, err := g.SetView("nextBtn", 0, 0, maxX, 4); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		fmt.Fprintln(v, "Button 1 - line 1")
+	}
 
+	if v, err := g.SetView("table", 0, 5, maxX, maxY); err != nil {
 		if len(subjectsList) < len(ids) {
 			ch := make(chan string)
 			getSubjects(ids, ch)
 			for subject := range ch {
-				fmt.Printf("> next: %v\n", subject)
+				fmt.Fprintf(v, "> next: %v\n", subject)
 				subjectsList = append(subjectsList, subject)
 
 				if len(ids) == len(subjectsList) {
@@ -96,7 +131,7 @@ func layout(g *gocui.Gui) error {
 			}
 		}
 
-		v.Frame = false
+		v.Frame = true
 
 		if err := v.SetOrigin(0, 0); err != nil {
 			return err
